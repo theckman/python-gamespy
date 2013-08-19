0d431227d41b830ea6cb6e61042aa0e7cc8952f9
Tim Heckman <tim+code@timheckman.net>
2013-08-18 23:28:39 -0400

    PEP 8 compliance issues

--- gamespy/__init__.py
+++ gamespy/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 from gamespy import exceptions
 from gamespy import protocol
 from gamespy import clients
--- gamespy/clients/generic.py
+++ gamespy/clients/generic.py
@@ -5,15 +5,15 @@ from twisted.internet import reactor
 
 __all__ = ['GenericGamespyClient']
 
+
 class GenericGamespyClient(object):
 
     def __init__(self, host, port):
         self.__host__ = host
         self.__port__ = port
         self.__proto__ = protocol.Gamespy(self.__host__,
-                                                  self.__port__)
+                                          self.__port__)
         self.__listener__ = reactor.listenUDP(0, self.__proto__)
-
 
     def query_server(self):
         reactor.callLater(2, reactor.stop)
