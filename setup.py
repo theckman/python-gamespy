68d89caab0df204c648486d59a9cb21dc248d922
Tim Heckman <tim+code@timheckman.net>
2013-08-27 20:19:39 -0400

    add setup.py to prep for cheeseshop

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gamespy',
+      version='0.1.0',
+      description='Python Gamespy Client',
+      author='Tim Heckman',
+      author_email='tim@timheckman.net',
+      url='https://github.com/theckman/python-gamespy',
+      packages=['gamespy', 'gamespy.clients'],
+     )
